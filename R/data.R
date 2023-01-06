ee7401c401bcafb13c8f42845e64a8cc88f1b024
Dan MacLean <maclean.daniel@gmail.com>
2023-01-06 16:00:39 +0000

    pheno_data

--- R/data.R
+++ R/data.R
@@ -6,5 +6,12 @@
 #' @format ## `who`
 #' A data frame with 14869 rows and 148 columns
 #'
-#' @source www.modencode.com
+#' @source www.modencode.org
 "count_dataframe"
+
+#' modencode phenotype data for Chapter 6 recipe 1
+#' @format ## `who`
+#' A data frame with 147 rows and 1 column
+#'
+#' @source www.modencode.org
+"pheno_data"
