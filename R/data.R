ff885028edf277fa98ff94061c4a941bd9aa424a
Dan MacLean <maclean.daniel@gmail.com>
2023-01-06 14:38:29 +0000

    initial

---
+++ R/data.R
@@ -0,0 +1,10 @@
+#' modencodefly transcript count data for Chapter 6 recipe 1
+#'
+#' A count dataframe of the modencodefly data from the NHGRI encyclopedia
+#' of DNA elements for Drosophila
+#'
+#' @format ## `who`
+#' A data frame with 14869 rows and 148 columns
+#'
+#' @source www.modencode.com
+"count_dataframe"
